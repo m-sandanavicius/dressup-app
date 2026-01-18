f0bcfa12964a320f054f36a7b70d2a7502ce07a8
Mindaugas Sandanavicius <m.sandanavicius@gmail.com>
2026-01-18 19:58:02 +0100

    Add login, log out buttons

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -9,19 +9,32 @@ import { APP_NAME } from '@/lib/constants';
 import { Metadata } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import CredentialsSignInForm from './credentials-sign-in-form';
+import { auth } from '@/auth';
+import { redirect } from 'next/navigation';
 
 export const metadata: Metadata = {
   title: 'Sign In',
 };
 
-export default function SignInPage() {
+export default async function SignInPage(props: {
+  searchParams: Promise<{ callbackUrl: string }>;
+}) {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || '/');
+  }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Card>
         <CardHeader className="space-y-4">
           <Link href="/" className="flex-center">
             <Image
-              src="/images/logo.svg"
+              src="/images/logo.png"
               width={100}
               height={100}
               alt={` ${APP_NAME} logo`}
@@ -33,7 +46,9 @@ export default function SignInPage() {
             Sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4"></CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   );
