a527b44b1098dbe1ae6a2f6c2518a76d5710de74
Mindaugas Sandanavicius <m.sandanavicius@gmail.com>
2026-01-18 21:24:29 +0100

    Add cart migrations and assign role to jwt

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,15 @@
 import { z } from 'zod';
-import { insertProductSchema } from '@/lib/validators';
+import {
+  cartItemSchema,
+  cartSchema,
+  insertProductSchema,
+} from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   CreatedAt: Date;
 };
+
+export type Cart = z.infer<typeof cartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
