113cd3128709639132dbca23e8ca7df70b851c34
Mindaugas Sandanavicius <m.sandanavicius@gmail.com>
2026-01-18 16:02:31 +0100

    Update logo icon

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="flex-start">
           <Link href="/" className="flex-start">
             <Image
-              src="/assets/logo.png"
+              src="/images/logo.png"
               alt={`${APP_NAME} logo`}
               height={48}
               width={48}
