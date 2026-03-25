61fd97f136666b7c027c6994d263f948800539db
Mindaugas Sandanavicius <m.sandanavicius@gmail.com>
2026-03-25 01:22:38 +0100

    Add admin products list page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,7 +2,7 @@
 
 import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 
 export async function getLatestProducts() {
   const data = await prisma.product.findMany({
@@ -17,4 +17,28 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug },
   });
+}
+
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const productsCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(productsCount / limit),
+  };
 }
